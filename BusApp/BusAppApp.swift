f3a06798d1ed9841f78cc902ec72d05c2e47e1a6
jhkim <biz.jh.kim@gmail.com>
2020-12-25 22:49:54 +0900

    Initial Commit

---
+++ BusApp/BusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusAppApp.swift
+//  BusApp
+//
+//  Created by 김재현 on 2020/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BusApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BusApp
+//
+//  Created by 김재현 on 2020/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
